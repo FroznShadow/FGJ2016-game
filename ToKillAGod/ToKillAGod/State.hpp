ade7b1ce3bc5691c90efb29dad39101767ae2873
Ormanus <osalminenster@gmail.com>
2016-01-29 20:37:18 +0200

    update state

--- ToKillAGod/ToKillAGod/State.hpp
+++ ToKillAGod/ToKillAGod/State.hpp
@@ -1,15 +1,36 @@
 #pragma once
 
+#include <vector>
+#include "GameObject.h"
+#include <SFML\Window.hpp>
+
 class State
 {
 public:
 	State();
 	virtual ~State();
-	virtual void update();
+	virtual void update(float dt)=0;
+	virtual void draw(sf::RenderWindow* window)=0;
+private:
+	std::vector<GameObject*> m_objects;
 };
 
 State::State() {}
 
 State::~State() {}
 
-void update() {}
+void State::update(float dt)
+{
+	for (auto it : m_objects)
+	{
+		it->M_update(dt);
+	}
+}
+
+void State::draw(sf::RenderWindow &window)
+{
+	for (auto it : m_objects)
+	{
+		it->M_draw(window);
+	}
+}
